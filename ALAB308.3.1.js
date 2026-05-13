418797c8ea9e8167cd2afd74b47986a1e5ed5b23
Quincy-Bee <quincybee@icloud.com>
2026-05-12 23:43:57 -0400

    part1

--- ALAB308.3.1.js
+++ ALAB308.3.1.js
@@ -1,35 +1,5 @@
 /* part 1: fizz buzz */
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
-}
-
-/*Fizz*/
-
-for (let i = 1; i <= 100; i++) {
-  //console.log(i);
-  if (i % 3 === 0) {
-    console.log("Fizz")
-  }
-
-}
-
-/*Buzz*/
-
-for (let i = 1; i <= 100; i++) {
-
-  if (i % 3 === 0) {
-    console.log("Fizz");
-  }
-
-  if (i % 5 === 0) {
-    console.log("Buzz");
-  }
-
-}
-
-
-
-for (let i = 1; i <= 100; i++) {
 
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("Fizz Buzz");
