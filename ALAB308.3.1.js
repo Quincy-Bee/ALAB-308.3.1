0b754b0709be1fdfc863aad7537a80d4155697d0
Quincy-Bee <quincybee@icloud.com>
2026-05-12 23:42:25 -0400

    second commit

--- ALAB308.3.1.js
+++ ALAB308.3.1.js
@@ -1,7 +1,47 @@
-let name = "Quincy";
+/* part 1: fizz buzz */
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
 
-console.log(name);
+/*Fizz*/
 
-for (let i = 0; i < 5; i++) {
-  console.log(name);
+for (let i = 1; i <= 100; i++) {
+  //console.log(i);
+  if (i % 3 === 0) {
+    console.log("Fizz")
+  }
+
+}
+
+/*Buzz*/
+
+for (let i = 1; i <= 100; i++) {
+
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+
+  if (i % 5 === 0) {
+    console.log("Buzz");
+  }
+
+}
+
+
+
+for (let i = 1; i <= 100; i++) {
+
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+
+  } else {
+    console.log(i);
+  }
+
 }
